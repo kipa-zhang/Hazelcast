d13f5f139e9dc38fd0f18af8a3d9684e526930e0
wgy <wang.guyi@wuxi.stee.stengg.com.cn>
2016-08-15 10:23:26 +0800

    3

--- HzaelcastTest2/src/com/kipa/app/HazelcastApplication.java
+++ HzaelcastTest2/src/com/kipa/app/HazelcastApplication.java
@@ -9,7 +9,7 @@ public class HazelcastApplication {
 	public static HazelcastInstance getHazelcastClientInstance(){
 		// 所有服务端需配置IP信息
 		ClientConfig clientConfig = new ClientConfig();
-		clientConfig.getNetworkConfig().addAddress("10.41.87.46");
+		clientConfig.getNetworkConfig().addAddress("10.41.87.46").addAddress("10.41.87.73");
 		HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
 		return hazelcastInstance;
 	}
